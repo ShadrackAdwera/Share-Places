f607ad4449d84adee50e5a09f164100467d74dec
FACTS  LIMITED <admin@factsafrica.com>
2020-06-10 14:19:06 +0300

    add a user item liast/page

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return(
+  const USERS = [
+    {
+      id: Math.random().toString(),
+      name: 'deez nuts',
+      image:
+        'https://images.unsplash.com/photo-1591756625939-2981cb54a3b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80',
+      places: 3,
+    },
+  ];
+
+  return (
     <div>
-    <h3>Users Works</h3>
-  </div>
-  )
+      <UsersList users={USERS} />
+    </div>
+  );
 };
 
 export default Users;
